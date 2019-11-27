087516f8bf48d33f07685871e14604601af20088
Justin Kirkwood <justin@corvuscreative.ca>
2019-11-27 15:35:26 -0800

    Upgrade from Polymer 2 to 3 via Polymer CLI Modulizer
    Utilized the Polymer CLI Modulizer to convert to javascript imports for Polymer 3

--- demo/px-code-design-demo.js
+++ demo/px-code-design-demo.js
@@ -1,4 +1,4 @@
-<!--
+/*
 Copyright (c) 2018, General Electric
 
 Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,36 +12,35 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
--->
+*/
+/* Common imports */
+/* Common demo imports */
+/* Demo DOM module */
+/* 6: Handle selectedOptions, update demo */
+/*
+  FIXME(polymer-modulizer): the above comments were extracted
+  from HTML and may be out of place here. Review them and
+  then delete this comment!
+*/
+import '@polymer/polymer/polymer-legacy.js';
 
-<!-- Common imports -->
-<link rel="import" href="../../polymer/polymer.html" />
-
-  <!-- Common demo imports -->
-  <link rel="import" href="../../px-sass-doc/px-sass-doc.html" />
-  <link rel="import" href="../css/px-code-design-demo-styles.html" />
-
-<!-- Demo DOM module -->
-<dom-module id="px-code-design-demo">
-  <template>
+import 'px-sass-doc/px-sass-doc.js';
+import '../css/px-code-design-demo-styles.js';
+import { Polymer } from '@polymer/polymer/lib/legacy/polymer-fn.js';
+import { html } from '@polymer/polymer/lib/utils/html-tag.js';
+Polymer({
+  _template: html`
   <!-- 0: Import the styles-->
   <style include="px-code-design-demo-styles" is="custom-style"></style>
 
 <!-- 1: Describe Module -->
-<px-sass-doc
-  module-name="px-code-design"
-  description="The Predix UI Code module provides styling for block and inline code snippets."
-  layer="base"
-  sassdoc-path="sassdoc.json"
-  dependencies='[
-    "https://github.com/PredixDev/px-forms-design",
-    "https://github.com/PredixDev/px-colors-design"
-  ]'
-  selected-options="{{selectedOptions}}"
-  tabs='["Default","Inline"]' selected-tab="{{selectedTab}}">
+<px-sass-doc module-name="px-code-design" description="The Predix UI Code module provides styling for block and inline code snippets." layer="base" sassdoc-path="sassdoc.json" dependencies="[
+    &quot;https://github.com/PredixDev/px-forms-design&quot;,
+    &quot;https://github.com/PredixDev/px-colors-design&quot;
+  ]" selected-options="{{selectedOptions}}" tabs="[&quot;Default&quot;,&quot;Inline&quot;]" selected-tab="{{selectedTab}}">
 
 <!-- FOR 'DEFAULT' TAB -->
-<template is="dom-if" if="[[_tabIs('Default',selectedTab)]]" restamp>
+<template is="dom-if" if="[[_tabIs('Default',selectedTab)]]" restamp="">
 
 <section slot="demo-html">
 
@@ -69,7 +68,7 @@ limitations under the License.
 </template>
 
 <!-- FOR 'OTHER' TAB -->
-<template is="dom-if" if="[[_tabIs('Inline',selectedTab)]]" restamp>
+<template is="dom-if" if="[[_tabIs('Inline',selectedTab)]]" restamp="">
 
 <section slot="demo-html">
 
@@ -78,7 +77,7 @@ Flag must be set before <code class="code code--inline">@import</code> in compon
 </section>
 
 <section slot="import">
-$inuit-enable-code--inline  : true;
+\$inuit-enable-code--inline  : true;
 @import 'px-code-design/_base.code.scss';
 </section>
 
@@ -88,35 +87,28 @@ $inuit-enable-code--inline  : true;
 <!-- 5: Set Usage HTML -->
 <section slot="usage">
 Use the following HTML to set up your project:
-```
+\`\`\`
 <!-- Code Block -->
 <figure class="code">
   <code>
-    &hellip;
+    …
   </code>
 </figure>
 
 
 <!-- Code Inline -->
-<code class="code code--inline">&hellip;</code>
+<code class="code code--inline">…</code>
 
-```
+\`\`\`
 </section>
 
 </px-sass-doc>
-</template>
+`,
 
-</dom-module>
+  is: 'px-code-design-demo',
 
-<!-- 6: Handle selectedOptions, update demo -->
-<script>
-  Polymer({
-    is: 'px-code-design-demo',
-
-    _tabIs : function(tabName) {
-      var selected = this.selectedTab || "";
-      return selected === tabName;
-    }
-
-  });
-</script>
+  _tabIs : function(tabName) {
+    var selected = this.selectedTab || "";
+    return selected === tabName;
+  }
+});
